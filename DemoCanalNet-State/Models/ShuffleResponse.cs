834a4398e68613ccdbd23e14053d451ebe28ea25
Gustavo Bellini Bigardi <gbbigardi@gmail.com>
2020-09-16 19:00:49 -0300

    commit com a demo inicial

---
+++ DemoCanalNet-State/Models/ShuffleResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DemoCanalNet_State.Models
+{
+    public class ShuffleResponse
+    {
+        public bool Success { get; set; }
+        [JsonPropertyName("deck_id")]
+        public string DeckId { get; set; }
+        public bool Shuffled { get; set; }
+        public int Remaining { get; set; }
+    }
+}
